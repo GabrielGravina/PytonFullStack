d35a503ec3640ab82bcccb9f1ce97ac5333e9f23
Gabriel Gravina <gabrielgravina01@gmail.com>
2024-10-10 20:10:16 -0300

    Setting up GET request

---
+++ backend/main.py
@@ -0,0 +1,21 @@
+# localhost:5000/create_contact
+
+# Request has a type: GET, POST, PATCH, DELETE...
+# We can pass JSON data. For example, we can pass what contact we want to delete.
+# Back-End returns a Responde. It has a status.
+
+from flask import request, jsonify
+from config import app, db
+from models import Contact
+
+@app.route("/contacts", methods=["GET"])
+def get_contacts():
+    contacts = Contact.query.all()
+    json_contacts = map(lambda x: x.to_json(), contacts)
+
+
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
